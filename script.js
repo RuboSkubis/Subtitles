ffb575b85877058a0332513afede201bff3769ac
Rubo120 <ruben.skubis8d@gmail.com>
2026-04-30 20:21:58 +0200

    .

--- script.js
+++ script.js
@@ -1,43 +1,33 @@
-    let arrayDeMarcasUno;
-    let arrayDeMarcasDos;
-    let regexp = /\d\d.\d\d.\d\d.\d\d\d\s...\s\d\d.\d\d.\d\d.\d\d\d/g;
-    let FirstOutPut = document.getElementById('output');
-    let SecondOutPut = document.getElementById("secOutPut");
-    let ThirdOutPut = document.getElementById('thirdOutPut');
-    let ForthOutPut = document.getElementById("forthOutPut");
+let arrayDeMarcasUno;
+let arrayDeMarcasDos;
+let regexp = /\d\d.\d\d.\d\d.\d\d\d\s...\s\d\d.\d\d.\d\d.\d\d\d/g;
+let FirstOutPut = document.getElementById('output');
+let SecondOutPut = document.getElementById("secOutPut");
+let ThirdOutPut = document.getElementById('thirdOutPut');
+let ForthOutPut = document.getElementById("forthOutPut");
 
-    document.getElementById('inputfile')
-      .addEventListener('change', function () {
+function readAndWrite(a, b, c,fichero) {
+  let fr = new FileReader();
 
-        let fr = new FileReader();
+  fr.onload = function () {
+    a.textContent = fr.result;
 
-        fr.onload = function () {
-          FirstOutPut.textContent = fr.result;
+    c = fr.result.match(regexp);
+    let textoPre = arrayDeMarcasUno.join("\n\n");
 
-          arrayDeMarcasUno = fr.result.match(regexp);
-          let textoPre = arrayDeMarcasUno.join("\n\n");
+    b.textContent = textoPre;
+  }
 
-          SecondOutPut.textContent=textoPre;
-        }
+  fr.readAsBinaryString(fichero.files[0]);
 
-        fr.readAsBinaryString(this.files[0]);
-       
-      });
+}
+document.getElementById('inputfile')
+  .addEventListener('change', function () {
+    readAndWrite(FirstOutPut,SecondOutPut,arrayDeMarcasUno,this);
+  });
 
 
-    document.getElementById('secFile')
-      .addEventListener('change', function () {
-
-        let fr = new FileReader();
-
-        fr.onload = function () {
-        ThirdOutPut.textContent = fr.result;
-
-        arrayDeMarcasDos = fr.result.match(regexp);
-        let textoPre = arrayDeMarcasDos.join("\n\n");
-
-        ForthOutPut.textContent=textoPre;
-        }
-
-        fr.readAsBinaryString(this.files[0]);
-      });
+document.getElementById('secFile')
+  .addEventListener('change', function () {
+    readAndWrite(ThirdOutPut,ForthOutPut,arrayDeMarcasDos,this);
+  });
