bf809f0411ecc2b4e57d45197b123ed38fcba928
Rubo120 <ruben.skubis8d@gmail.com>
2026-05-01 21:35:13 +0200

    Conseguido algoritmo para obtener marcas de inicio y final separadas

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-let arrayDeMarcasUno;
-let arrayDeMarcasDos;
+let marcasDeInicio;
+let marcasDeFinal;
 let regexp = /\d\d.\d\d.\d\d.\d\d\d\s...\s\d\d.\d\d.\d\d.\d\d\d/g;
+let regExpInicio = /\d{2}:\d{2}:\d{2},\d{3}./g;
+let regExpFinal = /.\d{2}:\d{2}:\d{2},\d{3}/g;
+// let regExpContenido = //g;
 let FirstOutPut = document.getElementById('output');
 let SecondOutPut = document.getElementById("secOutPut");
 let ThirdOutPut = document.getElementById('thirdOutPut');
 let ForthOutPut = document.getElementById("forthOutPut");
 
-function readAndWrite(a, b, c,fichero) {
+function readAndWrite(a, b, c, d, marcasDeInicio,marcasDeFinal,fichero) {
   let fr = new FileReader();
 
   fr.onload = function () {
     a.textContent = fr.result;
 
-    c = fr.result.match(regexp);
-    let textoPre = c.join("\n\n");
-
+    marcasDeInicio = fr.result.match(regExpInicio);
+    let textoPre = marcasDeInicio.map((item) => item.trim()).join("");
     b.textContent = textoPre;
+
+    marcasDeFinal = fr.result.match(regExpFinal);
+    textoPre = marcasDeFinal.map((item) => item.trim()).join("");
+    c.textContent = textoPre;
+
+    
   }
 
   fr.readAsBinaryString(fichero.files[0]);
@@ -23,11 +31,8 @@ function readAndWrite(a, b, c,fichero) {
 }
 document.getElementById('inputfile')
   .addEventListener('change', function () {
-    readAndWrite(FirstOutPut,SecondOutPut,arrayDeMarcasUno,this);
+    readAndWrite(FirstOutPut,SecondOutPut,ThirdOutPut,ForthOutPut,marcasDeInicio,marcasDeFinal,this);
   });
 
 
-document.getElementById('secFile')
-  .addEventListener('change', function () {
-    readAndWrite(ThirdOutPut,ForthOutPut,arrayDeMarcasDos,this);
-  });
+
