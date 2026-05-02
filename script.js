a5beaa60e99377d92eed9ed0e633f1f39f542d35
Rubo120 <ruben.skubis8d@gmail.com>
2026-05-02 04:48:52 +0200

    replanteandome lo de las regexp

--- script.js
+++ script.js
@@ -3,35 +3,57 @@ let marcasDeFinal;
 let regexp = /\d\d.\d\d.\d\d.\d\d\d\s...\s\d\d.\d\d.\d\d.\d\d\d/g;
 let regExpInicio = /\d{2}:\d{2}:\d{2},\d{3}./g;
 let regExpFinal = /.\d{2}:\d{2}:\d{2},\d{3}/g;
-// let regExpContenido = //g;
+
+let regExpContenido = /\s.+\s.*\s\s*/g;
 let FirstOutPut = document.getElementById('output');
 let SecondOutPut = document.getElementById("secOutPut");
 let ThirdOutPut = document.getElementById('thirdOutPut');
 let ForthOutPut = document.getElementById("forthOutPut");
 
-function readAndWrite(a, b, c, d, marcasDeInicio,marcasDeFinal,fichero) {
-  let fr = new FileReader();
+async function read(entrada) {
+  
 
-  fr.onload = function () {
-    a.textContent = fr.result;
+  let promesaDeResultado = new Promise(function (resolve) {
+    let fr = new FileReader();
 
-    marcasDeInicio = fr.result.match(regExpInicio);
-    let textoPre = marcasDeInicio.map((item) => item.trim()).join("");
-    b.textContent = textoPre;
 
-    marcasDeFinal = fr.result.match(regExpFinal);
-    textoPre = marcasDeFinal.map((item) => item.trim()).join("");
-    c.textContent = textoPre;
+    fr.onload = function () {
+      resolve(fr.result);
+      
+    }
 
-    
-  }
+    fr.readAsBinaryString(entrada.files[0]);
+  });
+  let result = await promesaDeResultado;
 
-  fr.readAsBinaryString(fichero.files[0]);
+  return result;
+}
+function write(a, b, c, d, resultado) {
+  a.textContent = resultado;
 
+  marcasDeInicio = resultado.match(regExpInicio);
+  let textoPre = marcasDeInicio.map((item) => item.trim()).join("\n\n");
+  b.textContent = textoPre;
+
+  marcasDeFinal = resultado.match(regExpFinal);
+  textoPre = marcasDeFinal.map((item) => item.trim()).join("\n\n");
+  c.textContent = textoPre;
+}
+function parseSRT(fichero){
+    let arrayDeContenido = fichero.split("\n");
+    console.log(arrayDeContenido);
+    console.log(arrayDeContenido.filter(item => item != ""));
 }
 document.getElementById('inputfile')
   .addEventListener('change', function () {
-    readAndWrite(FirstOutPut,SecondOutPut,ThirdOutPut,ForthOutPut,marcasDeInicio,marcasDeFinal,this);
+    let promesaDeResultado = read(this);
+
+    promesaDeResultado.then(
+      function(result){
+        write(FirstOutPut,SecondOutPut,ThirdOutPut,ForthOutPut,result);
+        parseSRT(result);
+      } 
+    );
   });
 
 
