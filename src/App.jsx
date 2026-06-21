7668fc7649a6a9c739e91dd74692f12af1989e25
Rubo120 <ruben.skubis8d@gmail.com>
2026-06-22 01:35:41 +0200

    se nos olvido quitar esto

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from './assets/vite.svg';
 import heroImg from './assets/hero.png';
-import { read, toMiliSeconds, toTimeStamp, parseSRT, unParseMergedSRT, write, eventMerge, download, addPersistence, optSubtitles, isContinuous } from './srtActions';
+import { read, toMiliSeconds, toTimeStamp, parseSRT, unParseMergedSRT, write, eventMerge, addPersistence, optSubtitles, isContinuous } from './srtActions';
 import './App.css';
 
 export default function App() {
